85d2bd99419f03f7f23fe53e74e2a144d1f405bf
sa9i9f <seyfff39@gmail.com>
2021-09-29 00:31:31 +0300

    complete my task solution

--- python-pass.py
+++ python-pass.py
@@ -4,4 +4,21 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        def check(l, r):
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        newString = ""
+        for c in range(len(s)):
+            temp = check(c, c)
+            if len(temp) > len(newString):
+                newString = temp
+            temp = check(c, c+1)
+            if len(temp) > len(newString):
+                newString = temp
+        return print("Output:", newString)
+
+
+s = input("Input: s = ")
+Solution.longest_palindromic(s)
